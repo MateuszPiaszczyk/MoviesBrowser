74018abbbe96116add678acb4be1a2d4d481a163
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-08 16:42:13 +0200

    added to folder core API keys

---
+++ src/core/App/apiCodes.js
@@ -0,0 +1,4 @@
+export const key = "ae7d4af255a05506e1ed3b49e48b0d5c";
+export const ApiPopularMovies = "https://api.themoviedb.org/3/movie/popular";
+export const ApiPopularPeople = "https://api.themoviedb.org/3/person/popular";
+export const url = "https://api.themoviedb.org/3";
