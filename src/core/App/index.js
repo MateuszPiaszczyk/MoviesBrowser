0c2e6838fbb006516e5612fb3309e4ef85fd934b
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-08 16:41:43 +0200

    added to app useState,useEffect

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,13 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-        <Normalize />
-        <GlobalStyle />
-        <div>Movies Browser</div>
+      <Normalize />
+      <GlobalStyle />
+      <div>Movies Browser</div>
     </ThemeProvider>
   );
 }
