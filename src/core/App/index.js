3d71f7e50c04101b3bbb451311f91d19af313b64
Aneta Borowski <borowski.aneta@gmail.com>
2023-05-08 22:28:10 +0200

    MovieList component added to App

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,13 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import { MovieList } from "../../features/MoviesList";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
         <Normalize />
         <GlobalStyle />
-        <div>Movies Browser</div>
+        <MovieList />
     </ThemeProvider>
   );
 }
