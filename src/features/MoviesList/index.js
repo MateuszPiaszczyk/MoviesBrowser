1448b2276f1f65cf4e000332756c080536fe0e77
Aneta Borowski <borowski.aneta@gmail.com>
2023-05-09 15:38:17 +0200

    mobileMax medai query added

--- src/features/MoviesList/index.js
+++ src/features/MoviesList/index.js
@@ -24,6 +24,125 @@ export const MovieList = () => {
                         </Rating>
                     </Content>
                 </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
+                <Tile>
+                    <Poster src={poster} alt="movie poster" />
+                    <Content>
+                        <Title>Mulan</Title>
+                        <Year>2020</Year>
+                        <Tags>
+                            <Tag>Action</Tag>
+                            <Tag>Adventure</Tag>
+                            <Tag>Drama</Tag>
+                        </Tags>
+                        <Rating>
+                            <Star />
+                            <Rate>7,8</Rate>
+                            <Votes>35 votes</Votes>
+                        </Rating>
+                    </Content>
+                </Tile>
             </List>
         </Container>
     )
--- src/features/MoviesList/styled.js
+++ src/features/MoviesList/styled.js
@@ -5,6 +5,12 @@ export const MainHeader = styled.h1`
     font-size: 36px;
     font-weight: 600;
     margin-top: 56px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 18px;
+        margin-top: 24px;
+        padding-left: 24px;
+    }
 `;
 
 export const List = styled.ul`
@@ -13,6 +19,12 @@ export const List = styled.ul`
     grid-gap: 24px;
     padding: 0;
     margin: 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        grid-template-columns: 1fr;
+        justify-items: center;
+        grid-gap: 16px;
+    }
 `;
 
 export const Tile = styled.div`
@@ -22,25 +34,48 @@ export const Tile = styled.div`
     box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
     border-radius: 5px;
     margin-bottom: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        max-width: 330px;
+        display: flex;
+        margin-bottom: 0;
+    }
 `;
 
 export const Poster = styled.img`
     width: 292px;
-    height: 434px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        max-width: 114px;
+        max-height: 169px;
+    }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin-left: 24px;
+    }
+`;
 
 export const Title = styled.h2`
     font-weight: 500;
     font-size: 22px;
     line-height: 29px;
     margin: 16px 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 16px;
+        margin: 0;
+    }
 `;
 
 export const Year = styled.div`
     font-size: 16px;
     color: ${({ theme }) => theme.colors.waterloo};
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 13px;
+    }
 `;
 
 export const Tags = styled.ul`
@@ -57,27 +92,50 @@ export const Tag = styled.li`
     font-size: 14px;
     margin-right: 8px;
     border-radius: 5px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 10px;
+        padding: 4px 8px;
+        margin-bottom: 12px;
+    }
 `;
 
 export const Rating = styled.section`
     display: flex;
     align-items: center;
     margin-top: 40px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin-top: 0;
+    }
 `;
 
 export const Star = styled(StarIcon)`
     width: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        width: 16px;
+    }
 `;
 
 export const Rate = styled.div`
     font-weight: 600;
     font-size: 16px;
     padding: 0 12px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 13px;
+        padding: 0 7px;
+    }
 `;
 
 export const Votes = styled.div`
     font-size: 16px;
     color: ${({ theme }) => theme.colors.waterloo};
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        font-size: 13px;
+    }
 `;
 
 
