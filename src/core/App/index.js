23cef93f7c5aee7e7f994f55ea11fd073e2015bd
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-10 21:22:23 +0200

    small update, navigation added

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -3,13 +3,15 @@ import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import { MovieList } from "../../features/MoviesList";
+import { Navigation } from "../../common/Navigation";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-        <Normalize />
-        <GlobalStyle />
-        <MovieList />
+      <Normalize />
+      <GlobalStyle />
+      <Navigation />
+      <MovieList />
     </ThemeProvider>
   );
 }
