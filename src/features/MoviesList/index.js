0a499d20614fafcefd0aa139a86d188cac479d2d
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-12 22:19:21 +0200

    cosmetic changes

--- src/features/MoviesList/index.js
+++ src/features/MoviesList/index.js
@@ -1,63 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Container } from "../../common/Container/styled";
 import {
-    Content,
-    Poster,
-    Tag,
-    Tags,
-    Title,
-    Tile,
-    Year,
-    Star,
-    Rating,
-    Rate,
-    Votes,
+  Content,
+  Poster,
+  Tag,
+  Tags,
+  Title,
+  Tile,
+  Year,
+  Star,
+  Rating,
+  Rate,
+  Votes,
 } from "../../common/MovieTile/styled";
 import { MainHeader, List } from "./styled";
 import poster from "../../common/img/poster.png";
 import { useEffect } from "react";
 import {
-    fetchPopularMovies,
-    selectPopularMoviesList,
-    selectPopularMoviesStatus,
+  fetchPopularMovies,
+  selectPopularMoviesList,
+  selectPopularMoviesStatus,
 } from "./popularMoviesSlice";
 
 export const MoviesList = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchPopularMovies());
-    }, []);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPopularMovies());
+  }, []);
 
-    const status = useSelector(selectPopularMoviesStatus);
-    const fetchResult = useSelector(selectPopularMoviesList);
-    if (status === "success") {
-        console.log(fetchResult.results);
+  const status = useSelector(selectPopularMoviesStatus);
+  const fetchResult = useSelector(selectPopularMoviesList);
+  if (status === "success") {
+    console.log(fetchResult.results);
 
-        return (
-            <Container>
-                <MainHeader>Popular Movies</MainHeader>
-                <List>
-                    {fetchResult.results.map((movie) => (
-                        <Tile key={movie.id}>
-                            <Poster src={poster} alt="movie poster" />
-                            <Content>
-                                <Title>{movie.title}</Title>
-                                <Year>2020</Year>
-                                <Tags>
-                                    <Tag>Action</Tag>
-                                    <Tag>Adventure</Tag>
-                                    <Tag>Drama</Tag>
-                                </Tags>
-                                <Rating>
-                                    <Star />
-                                    <Rate>7,8</Rate>
-                                    <Votes>35 votes</Votes>
-                                </Rating>
-                            </Content>
-                        </Tile>
-                    ))}
-                </List>
-            </Container>
-        )
-    }
+    return (
+      <Container>
+        <MainHeader>Popular Movies</MainHeader>
+        <List>
+          {fetchResult.results.map((movie) => (
+            <Tile key={movie.id}>
+              <Poster src={poster} alt="movie poster" />
+              <Content>
+                <Title>{movie.title}</Title>
+                <Year>2020</Year>
+                <Tags>
+                  <Tag>Action</Tag>
+                  <Tag>Adventure</Tag>
+                  <Tag>Drama</Tag>
+                </Tags>
+                <Rating>
+                  <Star />
+                  <Rate>7,8</Rate>
+                  <Votes>35 votes</Votes>
+                </Rating>
+              </Content>
+            </Tile>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 };
