26b1c7029376dcb3f838e326c80c21eb2d908439
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-12 22:16:01 +0200

    cosmetic changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './core/App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './core/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./core/App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./core/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>
-
-
 );
 
 // If you want to start measuring performance in your app, pass a function
