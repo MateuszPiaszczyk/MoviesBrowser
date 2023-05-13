393efad93f5c43c8bf905bbf4116527e5a7af452
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-13 18:30:42 +0200

    added to core genresSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { popularMoviesSaga } from "../features/MoviesList/popularMoviesSaga";
+import { GenresSaga } from "../features/MoviesList/Genres/genresSaga";
 
 export default function* rootSaga() {
-  yield all([popularMoviesSaga()]);
+  yield all([popularMoviesSaga(), GenresSaga()]);
 }
