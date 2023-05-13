0b4039095ca207386aab49f2e401ebd4478b32a3
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-13 18:31:46 +0200

    created genres slice,saga

---
+++ src/features/MoviesList/Genres/getGenres.js
@@ -0,0 +1,14 @@
+import { key, url } from "../../../core/apiCodes";
+
+export const getGenres = async () => {
+  try {
+    const response = await fetch(`${url}genre/movie/list?api_key=${key}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const genres = await response.json();
+    return genres;
+  } catch (error) {
+    console.error(error);
+  }
+};
