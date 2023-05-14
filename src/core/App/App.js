31cea5411987b65076e17460addbf3843862c782
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-14 16:43:20 +0200

    refactoring App.js

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,15 +1,12 @@
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { Navigation } from "../../common/Navigation";
-import {  MoviesList } from "../../features/MoviesList";
+import { MoviesList } from "../../features/MoviesList";
 import { GlobalStyle } from "./GlobalStyle";
 import { toMovie, toMovies, toPeople, toPerson } from "./routes";
-import { theme } from "./theme";
 
 export const App = () => (
   <HashRouter>
-    <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Navigation />
     <Normalize />
@@ -23,6 +20,5 @@ export const App = () => (
         <Redirect to={toMovies()} />
       </Route>
     </Switch>
-    </ThemeProvider>
   </HashRouter>
 );
