b0429f601eb6ccc44f8fea0e9ba4790dcb685dd1
Aneta Borowski <borowski.aneta@gmail.com>
2023-05-15 13:25:49 +0200

    styled added to PeopleList

---
+++ src/features/PeopleList/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 24px;
+  padding: 0;
+  margin: 0 0 24px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
+    grid-template-columns: repeat(2, 1fr);
+    justify-items: center;
+    grid-gap: 16px;
+  }
+`;
