563c03e6d68adfa6abd96d32b05750ed08ea05df
Aneta Borowski <borowski.aneta@gmail.com>
2023-05-15 13:23:25 +0200

    styles added to PersonTile component

---
+++ src/common/PersonTile/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+    background-color: ${({ theme }) => theme.colors.white};
+    max-width: 208px;
+    height: 340px;
+    padding: 16px;
+`;
+
+export const Photo = styled.img`
+    max-width: 177px;
+    border-radius: 5%;
+    margin-bottom: 16px;
+`;
+
+export const Name = styled.div`
+    font-weight: 500;
+    font-size: 22px;
+    text-align: center;
+`;
