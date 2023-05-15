1f96060b936ecfaaacbea6baa6eb2c90aa0c822a
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-15 21:16:13 +0200

    pre-activated search engine

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,12 +1,52 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { toMovies, toPeople } from "../../../core/App/routes";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../../../queryParameters";
+import { searchQueryParamName } from "../../../searchQueryParamName";
 import { SearchBox, SearchInput, StyledSearchIcon, Wrapper } from "./styled";
 
 export const Search = () => {
+  const queryURL = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
+  const location = useLocation();
+  const [isWaiting, setWaiting] = useState(true);
+  const [query, setQuery] = useState(queryURL);
+
+  const onInputChange = ({ target }) => {
+    setQuery(target.value);
+
+    if (isWaiting) {
+      setWaiting(false);
+      setTimeout(() => {
+        replaceQueryParameter({
+          basicURL: location.pathname.includes("movie")
+            ? toMovies()
+            : toPeople(),
+          key: searchQueryParamName,
+          value: target.value.trim() !== "" ? target.value : undefined,
+        });
+        setWaiting(true);
+      }, 500);
+    }
+  };
+
   return (
     <Wrapper>
       <SearchBox>
         <StyledSearchIcon />
       </SearchBox>
-      <SearchInput placeholder="Search for movies..." />
+      <SearchInput
+        value={query || ""}
+        placeholder={`${
+          location.pathname.includes("movie")
+            ? "Search for movies..."
+            : "Search for people..."
+        }`}
+        onChange={onInputChange}
+      />
     </Wrapper>
   );
 };
