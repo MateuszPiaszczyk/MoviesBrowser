4b26a6265bbaae1c12f0ff834383731f307bc93e
Dawid-Zieba <d.zieba2@gmail.com>
2023-05-19 13:40:40 +0200

    Movies Details added without working for now

---
+++ src/features/MoviesList/MovieDetails/index.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useQueryParameter } from "../../queryParameters";
+import { fetchMovieDetails, selectMovieDetails, selectMovieDetailsStatus,} from "./movieDetailsSlice";
+import { ErrorPage } from "../../../common/ErrorPage";
+
+export const MovieDetails = () => {
+    const dispatch= useDispatch();
+    const { id } = useParams(id);
+    const movieInfo = useSelector(selectMovieDetails);
+    const query = useQueryParameter("search")
+
+    useEffect(() => {
+        if (id) {
+            dispatch(fetchMovieDetails(id))
+        }
+    }, []);
+
+    console.log(movieInfo)
+    const status = useSelector(selectMovieDetailsStatus)
+    if (status ==="error") return <ErrorPage />
+    
+    return (
+        <>
+        
+        </>
+    )
+}
---
+++ src/features/MoviesList/MovieDetails/movieDetailsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieDetailsSlice = createSlice({
+    name:"movieDetails",
+    initialState: {
+        movieInfo: null,
+        status: null,
+    },
+
+    reducers: {
+        fetchMovieDetails: (_,{payload: id}) => ({
+            movieInfo: "loading",
+            status: null,
+            id
+            
+        }),
+        fetchMovieDetailsSuccess:(_,{payload:movieInfo}) => ({
+            status:"success",
+            movieInfo,
+        }),
+        fetchMovieDetailsError: () => ({
+            movieInfo: null,
+            status: "error",
+        }),
+    },
+});
+
+export const {
+    fetchMovieDetails,
+    fetchMovieDetailsSuccess,
+    fetchMovieDetailsError
+} = movieDetailsSlice.actions;
+
+export const selectMovieDetails = (state) => state.movieDetails;
+export const selectMovieDetailsStatus = (state) => selectMovieDetails(state).status;
+export const selectMovieInfo = (state) => selectMovieDetails(state).movieInfo;
+export const selectError = (state) => selectMovieDetails(state).error;
+
+export default movieDetailsSlice.reducer;
