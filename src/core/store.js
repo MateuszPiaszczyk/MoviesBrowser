577d7e70ad9abecf0ef6294dd01a19c51b709991
Aneta Borowski <borowski.aneta@gmail.com>
2023-05-21 13:57:15 +0200

    path to movieDetailsReducer updated

--- src/core/store.js
+++ src/core/store.js
@@ -4,7 +4,7 @@ import popularMoviesReducer from "../features/MoviesList/popularMoviesSlice";
 import popularPeopleReducer from "../features/PeopleList/popularPeopleSlice"
 import rootSaga from "./rootSaga";
 import genresReducer from "../features/MoviesList/Genres/genresSlice";
-import movieDetailsReducer from "../features/MoviesList/MovieDetails/movieDetailsSlice"
+import movieDetailsReducer from "../features/MovieDetails/movieDetailsSlice"
 
 
 const sagaMiddleware = createSagaMiddleware();
