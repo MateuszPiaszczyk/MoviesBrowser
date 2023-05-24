e2603419b6895c37ba24eb01232b8af3b719f044
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-24 21:54:15 +0200

    Container Removed

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -12,34 +12,31 @@ import {
 import camera from "../../assets/camera.svg";
 import { toMovies, toPeople } from "../../core/App/routes";
 import { Search } from "./Search";
-import { Container } from "../Container/styled";
 
 export const Navigation = () => {
   return (
     <StyledNavigation>
-      <Container>
-        <NavigationGrid>
-          <Wrapper>
-            <TitleLink to={toMovies()}>
-              <Icon src={camera} />
-              <Title>Movies Browser</Title>
-            </TitleLink>
-            <NavigationList>
-              <NavigationItem>
-                <StyledNavigationLink to={toMovies()}>
-                  Movies
-                </StyledNavigationLink>
-              </NavigationItem>
-              <NavigationItem>
-                <StyledNavigationLink to={toPeople()}>
-                  People
-                </StyledNavigationLink>
-              </NavigationItem>
-            </NavigationList>
-          </Wrapper>
-          <Search />
-        </NavigationGrid>
-      </Container>
+      <NavigationGrid>
+        <Wrapper>
+          <TitleLink to={toMovies()}>
+            <Icon src={camera} />
+            <Title>Movies Browser</Title>
+          </TitleLink>
+          <NavigationList>
+            <NavigationItem>
+              <StyledNavigationLink to={toMovies()}>
+                Movies
+              </StyledNavigationLink>
+            </NavigationItem>
+            <NavigationItem>
+              <StyledNavigationLink to={toPeople()}>
+                People
+              </StyledNavigationLink>
+            </NavigationItem>
+          </NavigationList>
+        </Wrapper>
+        <Search />
+      </NavigationGrid>
     </StyledNavigation>
   );
 };
