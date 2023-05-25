6e6ca210cd6d4b81f44c450edd93fccc941da135
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-25 16:28:43 +0200

    SearchResultPage improved

--- src/common/SearchResultPage/index.js
+++ src/common/SearchResultPage/index.js
@@ -1,8 +1,31 @@
+import { ErrorPage } from "../ErrorPage";
+import { Loading } from "../Loading";
+import { NoResult } from "../NoResult";
 import { OutputResultList, SearchInfo, Wrapper } from "./styled";
 
-export const SearchResultPage = ({ title }) => {
-  <Wrapper>
-    <SearchInfo>Search result for {title}</SearchInfo>
-    <OutputResultList />
-  </Wrapper>
+export const SearchResultPage = ({ status, title }) => {
+  switch (status) {
+    case "initial":
+      return null;
+
+    case "loading":
+      return <Loading />;
+
+    case "error":
+      return <ErrorPage />;
+
+    case "success":
+      return (
+        <Wrapper>
+          <SearchInfo>Search result for {title}</SearchInfo>
+          <OutputResultList />
+        </Wrapper>
+      );
+
+      case "noresult": 
+      return <NoResult />;
+
+    default:
+      throw new Error(`ENG: INCORRECT STATUS: ${status}`);
+  }
 };
