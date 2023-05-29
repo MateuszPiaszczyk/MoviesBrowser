e86aa6d07e9dbd067d8b7d1f209d6d6170c542e7
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-29 19:45:43 +0200

    rootsaga update

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,11 +1,12 @@
 import { all } from "redux-saga/effects";
 import { popularMoviesSaga } from "../features/movies/MoviesList/popularMoviesSaga";
 import { genresSaga } from "../features/movies/Genres/genresSaga";
-import { popularPeopleSaga} from "../features/people/PeopleList/popularPeopleSaga";
+import { popularPeopleSaga } from "../features/people/PeopleList/popularPeopleSaga";
 import { movieDetailsSaga } from "../features/movies/MovieDetails/movieDetailsSaga";
+
 export default function* rootSaga() {
   yield all([
-    popularMoviesSaga(), 
+    popularMoviesSaga(),
     genresSaga(),
     popularPeopleSaga(),
     movieDetailsSaga(),
