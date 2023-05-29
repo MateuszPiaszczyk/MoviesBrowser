910e00c23e9763ead6bd495631f64f0f51a2082f
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-05-29 19:44:20 +0200

    moviedetails saga updated

--- src/features/movies/MovieDetails/movieDetailsSaga.js
+++ src/features/movies/MovieDetails/movieDetailsSaga.js
@@ -1,22 +1,23 @@
-import { takeLatest, delay, call, put } from "redux-saga/effects";
+import { select, call, put } from "redux-saga/effects";
 import {
-  fetchMovieDetails,
-  fetchMovieDetailsSuccess,
+  selectMovieId,
   fetchMovieDetailsError,
+  fetchMovieDetailsSuccess,
+  getMovieId,
 } from "./movieDetailsSlice";
-import { getMovieDetails } from "./getMovieDetails";
-
-function* fetchMovieDetailsHandler({ payload }) {
-  try {
-    const movieInfo = yield call(getMovieDetails, payload);
-    yield delay(1000);
-    yield put(fetchMovieDetailsSuccess(movieInfo));
-  } catch (error) {
-    yield put(fetchMovieDetailsError());
-    yield call(alert, "Download failed");
-  }
-}
+import { getMovieDetails, getMovieCredits } from "./getMovieDetails";
 
 export function* movieDetailsSaga() {
-  yield takeLatest(fetchMovieDetails.type, fetchMovieDetailsHandler);
+  try {
+    const id = yield select(selectMovieId);
+    if (!id || id === 0) {
+      // Jeśli movieId jest puste lub nieprawidłowe, pobierz poprawne id przed pobraniem szczegółów
+      yield put(getMovieId()); // Wywołaj akcję pobrania prawidłowego identyfikatora filmu
+    }
+    const details = yield call(getMovieDetails, id);
+    const credits = yield call(getMovieCredits, id);
+    yield put(fetchMovieDetailsSuccess({ details, credits }));
+  } catch (error) {
+    yield put(fetchMovieDetailsError());
+  }
 }
