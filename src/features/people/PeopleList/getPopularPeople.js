0f331a2a0d1b493a4c28bac8a3eb52b83ab0e276
Dawid-Zieba <d.zieba2@gmail.com>
2023-06-02 15:39:42 +0200

    changed from fetch to axios

--- src/features/people/PeopleList/getPopularPeople.js
+++ src/features/people/PeopleList/getPopularPeople.js
@@ -1,16 +1,14 @@
-import { API_KEY, ApiPopularPeople } from "../../../core/apiCodes";
+import axios from "axios";
+const API_KEY = "ae7d4af255a05506e1ed3b49e48b0d5c";
+const API_URL = "https://api.themoviedb.org/3";
 
-export const getPopularPeople = async () => {
-  try {
-    const response = await fetch(
-      `${ApiPopularPeople}?api_key=${API_KEY}&language=en-US&page=1`
-    );
-    if (!response.ok) {
-      throw new Error("not found movies.");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
+export const getPopularPeople = async ({ page }) => {
+  const response = await axios.get(
+    `${API_URL}/person/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+  );
+  if (!response.ok) {
+    new Error(response.statusText);
   }
+
+  return await response.data;
 };
