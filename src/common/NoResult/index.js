af68cdb0fbf6f5518beedbcfaa5790726ea7a5ad
Aneta Borowski <borowski.aneta@gmail.com>
2023-06-02 23:22:47 +0200

    NoResult page fixed

--- src/common/NoResult/index.js
+++ src/common/NoResult/index.js
@@ -2,9 +2,9 @@ import { Wrapper, NoResultIcon} from "./styled";
 import { Container } from "../Container/styled";
 import { MainHeader } from "../MainHeader";
 
-export const NoResult = () => (
+export const NoResult = ({query}) => (
   <Container>
-    <MainHeader tite="Sorry,there are no results for this movie." />
+    <MainHeader title={`Sorry,there are no results for "${query}"`} />
     <Wrapper>
       <NoResultIcon />
     </Wrapper>
--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -43,7 +43,7 @@ export const MoviesList = () => {
     <>
     {pageNumber > totalPages ?
       <ErrorPage /> : totalResults === 0 ?
-      <NoResult /> :
+      <NoResult query={query} /> :
       
       <Container>
         <MainHeader title={
