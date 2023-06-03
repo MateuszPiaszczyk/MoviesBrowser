eec4598d4aefeeac814bb09f8df43d87216eb99d
Aneta Borowski <borowski.aneta@gmail.com>
2023-06-03 23:13:26 +0200

    movieDetails cleaned

--- src/features/movies/MovieDetails/movieDetailsSlice.js
+++ src/features/movies/MovieDetails/movieDetailsSlice.js
@@ -38,7 +38,6 @@ export const selectStatus = (state) => selectMovieState(state).status;
 export const selectDetails = (state) => selectMovieState(state).details;
 export const selectCredits = (state) => selectMovieState(state).credits;
 export const selectMovieId = (state) => selectMovieState(state).movieId;
-export const selectCrew = (state) => selectMovieState(state).crew;
-export const selectCast = (state) => selectMovieState(state).cast;
 export const selectMovie = (state) => selectMovieState(state).movie;
+
 export default movieDetailsSlice.reducer;
